54dcbb308b6603580be02cfaa7f6617f2fe547f9
siriwatknp <siriwatkunaporn@gmail.com>
2021-06-16 16:13:20 +0700

    fix ButtonBase related components focus update

--- packages/material-ui/src/Fab/Fab.test.js
+++ packages/material-ui/src/Fab/Fab.test.js
@@ -113,8 +113,9 @@ describe('<Fab />', () => {
     );
     const button = getByRole('button');
 
-    act(() => {
-      fireEvent.keyDown(document.body, { key: 'TAB' });
+    fireEvent.keyDown(document.body, { key: 'TAB' });
+
+    await act(async () => {
       button.focus();
     });
 
