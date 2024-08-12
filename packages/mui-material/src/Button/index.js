2fb9d251c6abdc6c1e1d61e573c841d4a59636f6
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-12 16:53:17 +0700

    wip: add root slot to Button and split into md2, md3

--- packages/mui-material/src/Button/index.js
+++ packages/mui-material/src/Button/index.js
@@ -1,5 +1,6 @@
 'use client';
 export { default } from './Button';
+export { ButtonRoot } from './Button';
 
 export { default as buttonClasses } from './buttonClasses';
 export * from './buttonClasses';
