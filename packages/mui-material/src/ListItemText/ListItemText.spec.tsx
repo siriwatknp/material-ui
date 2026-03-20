90f189a34290881af1ef4ac0c4e86c70710afe3f
siriwatknp <siriwatkunaporn@gmail.com>
2026-03-20 15:35:47 +0700

    restore all spec tests, replace deprecated props with slotProps

--- packages/mui-material/src/ListItemText/ListItemText.spec.tsx
+++ packages/mui-material/src/ListItemText/ListItemText.spec.tsx
@@ -1,7 +1,28 @@
 import * as React from 'react';
 import { ListItemText } from '@mui/material';
 
-function slotPropsTest() {
+const CustomComponent: React.FC<{ prop1: string; prop2: number }> = function CustomComponent() {
+  return <div />;
+};
+
+function typographyPropsTest() {
+  // @ts-expect-error
+  <ListItemText slotProps={{ primary: { component: 'incorrectComponent' } }} />;
+  <ListItemText slotProps={{ primary: { component: 'a', href: 'href' } }} />;
+  <ListItemText
+    slotProps={{
+      primary: {
+        component: 'a',
+        // @ts-expect-error
+        htmlFor: 'nonexistent-attribute',
+      },
+    }}
+  />;
+  <ListItemText
+    slotProps={{ primary: { component: CustomComponent, prop1: 'prop1', prop2: 2 } }}
+  />;
+  // @ts-expect-error
+  <ListItemText slotProps={{ primary: { component: CustomComponent, prop2: 2 } }} />;
   <ListItemText slotProps={{ primary: { variant: 'h1' } }} />;
   <ListItemText slotProps={{ primary: { align: 'left' } }} />;
   <ListItemText
@@ -15,6 +36,26 @@ function slotPropsTest() {
       },
     }}
   />;
+}
+
+function secondaryTypographyPropsTest() {
+  // @ts-expect-error
+  <ListItemText slotProps={{ secondary: { component: 'incorrectComponent' } }} />;
+  <ListItemText slotProps={{ secondary: { component: 'a', href: 'href' } }} />;
+  <ListItemText
+    slotProps={{
+      secondary: {
+        component: 'a',
+        // @ts-expect-error
+        htmlFor: 'nonexistent-attribute',
+      },
+    }}
+  />;
+  <ListItemText
+    slotProps={{ secondary: { component: CustomComponent, prop1: 'prop1', prop2: 2 } }}
+  />;
+  // @ts-expect-error
+  <ListItemText slotProps={{ secondary: { component: CustomComponent, prop2: 2 } }} />;
   <ListItemText slotProps={{ secondary: { variant: 'h1' } }} />;
   <ListItemText slotProps={{ secondary: { align: 'left' } }} />;
   <ListItemText
@@ -28,10 +69,49 @@ function slotPropsTest() {
       },
     }}
   />;
+}
+
+function mixedTypographyPropsTest() {
   <ListItemText
     slotProps={{
-      primary: { variant: 'h1' },
-      secondary: { variant: 'h1' },
+      // @ts-expect-error
+      primary: { component: 'incorrectComponent' },
+      // @ts-expect-error
+      secondary: { component: 'incorrectComponent' },
+    }}
+  />;
+  <ListItemText
+    slotProps={{
+      primary: { component: 'a', href: 'href' },
+      secondary: { component: 'a', href: 'href' },
+    }}
+  />;
+  <ListItemText
+    slotProps={{
+      primary: {
+        component: 'a',
+        // @ts-expect-error
+        htmlFor: 'nonexistent-attribute',
+      },
+      secondary: {
+        component: 'a',
+        // @ts-expect-error
+        htmlFor: 'nonexistent-attribute',
+      },
+    }}
+  />;
+  <ListItemText
+    slotProps={{
+      primary: { component: CustomComponent, prop1: 'prop1', prop2: 2 },
+      secondary: { component: CustomComponent, prop1: 'prop1', prop2: 2 },
+    }}
+  />;
+  <ListItemText
+    slotProps={{
+      // @ts-expect-error
+      primary: { component: CustomComponent, prop2: 2 },
+      // @ts-expect-error
+      secondary: { component: CustomComponent, prop2: 2 },
     }}
   />;
 }
